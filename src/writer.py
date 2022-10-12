6be2b790d3a0c8aee9ab341940ebe3af300aa9a4
arjun289 <arjun289singh@gmail.com>
2022-10-12 15:38:24 +0800

    Make mods for writer file1

--- src/writer.py
+++ src/writer.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -5,13 +6,13 @@ import pyarrow.parquet as pq
 from db_reader import create_connection
 
 sensor_schema = pa.schema([
-    ('inserted_timestamp', pa.timestamp('s',  tz='UTC')),
     ('org_id', pa.int32()),
     ('project_id', pa.int32()),
     ('sensor_id', pa.int32()),
     ('parameter_uuid', pa.string()),
+    ('inserted_timestamp', pa.timestamp('ms')),
     ('value', pa.float32()),
-    ('inserted_at', pa.timestamp('s',  tz='UTC'))
+    ('inserted_at', pa.timestamp('ms'))
 ])
 
 
@@ -19,18 +20,23 @@ if __name__ == '__main__':
     connection = create_connection("acqdat_core_dev", "postgres", "postgres", "188.166.230.56", "5431")
 
     cursor = connection.cursor()
-    query = "select * from acqdat_sensors_data limit 10"
+    query = "select * from acqdat_sensors_data limit 10000000"
 
     cursor.execute(query)
     records = cursor.fetchall()
+    print(records)
 
     result = []
-    for row in records: 
+    for row in records:
         flat_array = []
         for key in row[4]:
-            flat_array = [(row[0], row[1], row[2], row[3], key['uuid'], key['value'], row[5])]
+            flat_array = [ row[1], row[2], row[3], key['uuid'], row[0].replace(tzinfo=None), float(key['value']), row[5].replace(tzinfo=None)]
+            # flat_array = [ row[1], row[2], row[3], key['uuid'], key['value']]
         result.append(flat_array)
 
+    dataframe = pd.DataFrame(result, columns=['org_id', 'project_id', 'sensor_id', 'parameter_uuid', 'inserted_timestamp', 'value', 'inserted_at'])
 
-    dataframe = pd.DataFrame(result)
     print(dataframe)
+
+    sensor_table = pa.Table.from_pandas(dataframe)
+    pq.write_table(sensor_table, 'data/sensor_data.parquet', version='1.0')
